53001f6fb82251f546d61244cc378a7db08804b1
shihadshadhu <shadhunihal55@gmail.com>
2023-03-13 15:31:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,95 @@
+// //datatypes
+// var x = 20;
+// {
+//     var x=10;
+//     varsum=5+10;
+//     console.log(x);
+// }
+// console.log(x);
+// const pi =3.14;
+// var sum =5+10;
+// console.log(sum);
+// var sub =10-4;
+// console.log(sub);
+// var mul =5*3;
+// console.log(mul);
+// document.write("sum ="+sum);
+// document.write("mul ="+mul);
+// document.write("sub ="+sub);
+// var fname="Shihad";
+// var lname="P.N";
+// document.write(`my full name is ${fname} ${lname}`);
+// var students={
+//     name:"shihad",
+//     age:10,
+//     place:"irinjalakuda"
+// }
+// console.log(students);
+// console.log(students.name,students.place);
+// var cars={
+//     model:"BMW",
+//     price:15000000,
+//     color:"white"
+// }
+// console.log(cars);
+// console.log(cars.model,cars.price);
+/*var fruits=["apple","banana","orange",10];
+fruits[2]="grapes"
+fruits.push("plum")
+fruits.unshift("apple")
+console.log(fruits.length);
+fruits.pop("plum");
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+var person=[
+    {
+        name:"shihad",
+        age:"10",
+        place:"irinjalakuda"
+    },
+    {
+        name:"ashif",
+        age:"20",
+        place:"chalakudy"
+    }
+]
+console.log("shihad");
+function add(a,b){
+    var c = a+b;
+    
+    console.log(c)
+}
+add(1,2);
+//add(3,4);*/
+/*const addition =(a,b)=>{
+    var c=a+b;
+    console.log(c)
+}
+addition(2,3)*/
+/*var x= 20;
+if(x>20){
+    console.log("good");
+}
+else
+console.log("bad");*/
+/*for(var i=1;i<=5;i++)
+{
+    console.log(i);
+}*/
+/*var fruits =["apple","orange","mango","grapes"]
+1  1<4 2
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i])
+}
+let p=0;
+while(p<fruits.length){
+    console.log(fruits[p]);
+    p++;
+}*/
+var fruits=["apple","banana","orange"];
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
